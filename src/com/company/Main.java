3b52b32e37bd81bd92e43163baef8b2dceaa3b8b
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-02-22 18:41:40 +0200

    clean package

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,11 @@
 package com.company;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Runnables.class);
 
     public static void main(String[] args) {
-
         logger.info("Program is started");
         Counter counter = new Counter();
         Thread first = new Threads(counter);
